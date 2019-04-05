c9a87c01fa71ff475f0c2239e0cc42f2996ca252
Osman Yousufi <oyousufi@lcs.on.ca>
2019-04-05 09:25:01 -0400

    added a picture of yolk and change the letter to capital Y

--- EGGSPLOSION/yolk.java
+++ EGGSPLOSION/yolk.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class yolk extends Actor
+public class Yolk extends Actor
 {
     /**
      * Act - do whatever the yolk wants to do. This method is called whenever
