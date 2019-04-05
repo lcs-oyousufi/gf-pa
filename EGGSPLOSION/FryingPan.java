e5ec8938aaf0893764d023edcc60aca92b2dc5b2
Osman Yousufi <oyousufi@lcs.on.ca>
2019-04-05 09:19:15 -0400

    added some subclasses

---
+++ EGGSPLOSION/FryingPan.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FryingPan here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FryingPan extends Mover
+{
+    /**
+     * Act - do whatever the FryingPan wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ EGGSPLOSION/lightning.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class lightning here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class lightning extends Mover
+{
+    /**
+     * Act - do whatever the lightning wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
